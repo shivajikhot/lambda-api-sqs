2f811f56f3acba266c045b7e9da62063422f8dec
shivajikhot <shivajkv@gmail.com>
2025-02-23 16:26:17 +0530

    Update variables.tf

--- infra/modules/apigateway/variables.tf
+++ infra/modules/apigateway/variables.tf
@@ -4,13 +4,6 @@ variable "tag_environment" {
     type = string
 }
 
-variable "api_gateway_greeting_queue_role_arn" {
-    type = string
-}
-
-variable "greeting_queue_name" {
-    type = string
-}
 variable "greeting_lambda.invoke_arn" {
     type = string
 }
