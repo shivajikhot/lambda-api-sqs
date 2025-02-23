b0cd2961002216a05466882f547454319f0be824
shivajikhot <shivajkv@gmail.com>
2025-02-23 14:32:44 +0530

    Create variables.tf

---
+++ infra/modules/apigateway/variables.tf
@@ -0,0 +1,13 @@
+
+
+variable "tag_environment" {
+    type = string
+}
+
+variable "api_gateway_greeting_queue_role_arn" {
+    type = string
+}
+
+variable "greeting_queue_name" {
+    type = string
+}
