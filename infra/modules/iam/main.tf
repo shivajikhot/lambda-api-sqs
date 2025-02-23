8e98da696c739f17446f605b2b28bdfb771963d6
shivajikhot <shivajkv@gmail.com>
2025-02-23 12:54:23 +0530

    Update main.tf

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -55,3 +55,24 @@ resource "aws_iam_role_policy_attachment" "s3_full_access_attachment" {
   policy_arn = aws_iam_policy.lambda_s3_access_policy.arn
   role       = aws_iam_role.lambda_execution_role.name
 }
+###################################################
+resource "aws_iam_policy" "greeting_lambda_sqs_policy" {
+  name        = "greeting_lambda_sqs_policy"
+  description = "Grants access to read messages from SQS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMEssage", "sqs:GetQueueAttributes"],
+        Effect   = "Allow",
+        Resource = [var.greeting_queue_arn]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "greeting_lambda_sqs_policy_attachment" {
+  policy_arn = aws_iam_policy.greeting_lambda_sqs_policy.arn
+  role       = aws_iam_role.greeting_lambda_execution_role.name
+}
