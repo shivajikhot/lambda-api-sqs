6e408986696b505655c8a1421ee5b716d0a4662e
shivajikhot <shivajkv@gmail.com>
2025-02-23 11:54:55 +0530

    Create main.tf

---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,49 @@
+# Execution role
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "terraform-lambda-greetings-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+# Access policy
+resource "aws_iam_policy" "lambda_s3_access_policy" {
+  name        = "terraform-lambda-s3-access-policy"
+  description = "Grants access to source and destination buckets"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["s3:GetObject"],
+        Effect   = "Allow",
+        Resource = [
+          "${aws_s3_bucket.src_bucket.arn}/*"
+        ]
+      },{
+        Action   = ["s3:PutObject"],
+        Effect   = "Allow",
+        Resource = [
+          "${aws_s3_bucket.dst_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attaches the policy to the role
+resource "aws_iam_role_policy_attachment" "s3_full_access_attachment" {
+  policy_arn = aws_iam_policy.lambda_s3_access_policy.arn
+  role       = aws_iam_role.lambda_execution_role.name
+}
