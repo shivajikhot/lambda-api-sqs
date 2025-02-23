824babbb9893a91b215d3550dd7b84e74f667c92
shivajikhot <shivajkv@gmail.com>
2025-02-23 22:37:05 +0530

    Update main.tf

--- infra/modules/apigateway/main.tf
+++ infra/modules/apigateway/main.tf
@@ -92,6 +92,7 @@ resource "aws_api_gateway_stage" "greeting_api_stage" {
   stage_name    = var.tag_environment
   rest_api_id   = aws_api_gateway_rest_api.greeting_api.id
   deployment_id = aws_api_gateway_deployment.greeting_api_deployment.id
+  xray_tracing_enabled = true
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gateway_log_group.arn
