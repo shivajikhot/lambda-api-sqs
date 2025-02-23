c50cc737a8ca9b90ea45f302f9c11d4090eca373
shivajikhot <shivajkv@gmail.com>
2025-02-23 14:34:24 +0530

    Update output.tf

--- infra/modules/iam/output.tf
+++ infra/modules/iam/output.tf
@@ -1,3 +1,6 @@
 output "lambda_execution_role_arn" {
   value = aws_iam_role.lambda_execution_role.arn
 }
+output "api_gateway_greeting_queue_role_arn" {
+  value = aws_iam_role.api_gateway_greeting_queue_role.arn
+}
