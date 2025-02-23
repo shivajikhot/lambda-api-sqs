21a01076f8da33f00dab364e7d44bcbe41fe3ef9
shivajikhot <shivajkv@gmail.com>
2025-02-23 12:09:43 +0530

    Create main.tf

---
+++ infra/modules/lambda/main.tf
@@ -0,0 +1,26 @@
+# Create a zip file with function code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.root}/../app/index.mjs"
+  output_path = "lambda.zip"
+}
+
+# Create a Lambda function
+resource "aws_lambda_function" "greeting_lambda" {
+  function_name = "greetings-lambda-function"
+
+  handler     = "index.handler"
+  runtime     = "nodejs18.x"
+  memory_size = var.lambda_memory_size
+  role        = var.lambda_execution_role_arn
+
+  environment {
+    variables = {
+      SRC_BUCKET = var.src_bucket_id,
+      DST_BUCKET = var.dst_bucket_id
+    }
+  }
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
