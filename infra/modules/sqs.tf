4a176beae9081ddb85a407323077a93c6ba4d97d
shivajikhot <shivajkv@gmail.com>
2025-02-23 12:45:26 +0530

    Create sqs.tf

---
+++ infra/modules/sqs.tf
@@ -0,0 +1,10 @@
+
+
+resource "aws_sqs_queue" "greeting_queue" {
+  name                    = "greetings_queue"
+  sqs_managed_sse_enabled = true
+  
+  tags = {
+    environment: var.tag_environment
+  }
+}
