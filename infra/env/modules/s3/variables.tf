09972cbe9be6ed346441e364b620e97f3d3f38dd
shivajikhot <shivajkv@gmail.com>
2025-02-23 11:20:13 +0530

    Update variables.tf

--- infra/env/modules/s3/variables.tf
+++ infra/env/modules/s3/variables.tf
@@ -2,3 +2,6 @@ variable "bucket_names" {
   type    = list(string)  
   default = ["src-bucket-some-random-string", "dst-bucket-some-random-string"]  
 }  
+variable "tag_environment" {
+  type = string
+}
